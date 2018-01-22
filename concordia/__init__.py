2b5e988e85dda9c6309c8ed53b2bd668ccb5d60d
ClimbsRocks <climbsbytes@gmail.com>
2018-01-21 22:10:26 -0800

    makes imports easier when modifying source code

--- concordia/__init__.py
+++ concordia/__init__.py
@@ -1,2 +1,2 @@
-from concordia.Concordia_logic import Concordia, load_concordia
-from concordia._version import __version__
+from .Concordia_logic import Concordia, load_concordia
+from ._version import __version__
